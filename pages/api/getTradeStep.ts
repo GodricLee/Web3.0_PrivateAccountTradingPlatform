e5a2d836eb0525d34e42799589706ef31faae492
GodricLee <44858594+GodricLee@users.noreply.github.com>
2024-11-20 15:45:35 +0800

    fixed getTradeStep.ts

--- pages/api/getTradeStep.ts
+++ pages/api/getTradeStep.ts
@@ -16,14 +16,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     // 初始化 Moralis
-    await Moralis.start({ apiKey: process.env.MORALIS_API_KEY || '' });
+    if (!Moralis.Core.isStarted) {
+      await Moralis.start({ apiKey: process.env.MORALIS_API_KEY || '' });
+    }
 
     // 调用智能合约 get_current_step 方法
     const step = await Moralis.EvmApi.utils.runContractFunction({
       chain: '0xa4b1', // 替换为链 ID
       address: process.env.CONTRACT_ADDRESS || '', // 替换为合约地址
       functionName: 'get_current_step',
-      abi: process.env.CONTRACT_ABI || '',
+      abi: process.env.CONTRACT_ABI ? JSON.parse(process.env.CONTRACT_ABI) : [],
       params: {
         new_key: '114514',
         requester_address: '0xeE7e2c107AbC589Da98f642829054928AeD9FEf5', // 替换为用户地址
